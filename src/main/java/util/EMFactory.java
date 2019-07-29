eb1dc06cc2e89de539ddf9eaea89d67b08aeeac0
vivialmeida <viviane_k5@hotmail.com>
2019-07-29 17:10:58 -0300

    Identificacao das chaves primarias e tabelas na classe cliente

---
+++ src/main/java/util/EMFactory.java
@@ -0,0 +1,25 @@
+package util;
+
+public class JpaUtil {
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+    public class EMFactory {
+
+        private static final EntityManagerFactory factory =
+                Persistence.createEntityManagerFactory("pedidovendas");
+
+
+        public EntityManager getEntityManager() {
+            return factory.createEntityManager();
+
+        }
+
+        public void close() {
+            factory.close();
+        }
+    }
+}
